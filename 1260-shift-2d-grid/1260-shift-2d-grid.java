e8d96b087828934e2f6911052ca869d632ebe10e
Vinay Kushwah <vk0475020@gmail.com>
2026-07-21 01:07:46 +0530

    Time: 4 ms (97.01%), Space: 47.2 MB (44.14%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,44 +1,56 @@
 import java.util.*;
 
 class Solution {
-
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
 
-        int row = grid.length;
-        int col = grid[0].length;
-        int total = row * col;
+        int m = grid.length;
+        int n = grid[0].length;
+        int size = m * n;
 
-        k = k % total;
+        k = k % size;
 
-        reverse(grid, 0, total - 1, col);
-        reverse(grid, 0, k - 1, col);
-        reverse(grid, k, total - 1, col);
+        // Step 1: Convert 2D -> 1D
+        int[] arr = new int[size];
+        int index = 0;
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[index++] = grid[i][j];
+            }
+        }
+
+        // Step 2: Rotate array
+        reverse(arr, 0, size - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, size - 1);
+
+        // Step 3: Convert 1D -> 2D
+        index = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = arr[index++];
+            }
+        }
+
+        // Step 4: Convert to List<List<Integer>>
         List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < row; i++) {
-            List<Integer> list = new ArrayList<>();
-
-            for (int j = 0; j < col; j++) {
-                list.add(grid[i][j]);
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(grid[i][j]);
             }
-
-            ans.add(list);
+            ans.add(row);
         }
 
         return ans;
     }
 
-    public void reverse(int[][] grid, int left, int right, int col) {
-
+    public void reverse(int[] arr, int left, int right) {
         while (left < right) {
-
-            int temp = grid[left / col][left % col];
-
-            grid[left / col][left % col] = grid[right / col][right % col];
-
-            grid[right / col][right % col] = temp;
-
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             left++;
             right--;
         }
