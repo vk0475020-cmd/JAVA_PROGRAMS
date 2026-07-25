15c75fd5894baa34f7f8f651a281523265c5d4ef
Vinay Kushwah <vk0475020@gmail.com>
2026-07-25 22:48:55 +0530

    Time: 1 ms (99.96%), Space: 47 MB (27.85%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
         for(int i=0;i<nums.length;i++){
-            ans=ans^nums[i];//xor of same number is zero
+            ans=ans^nums[i];//xor of same number is zero ak dusre ko cencle kar dete hai
         }
         return ans;
     }
