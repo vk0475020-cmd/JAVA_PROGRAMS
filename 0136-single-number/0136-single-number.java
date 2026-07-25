a3f8d06d22dd910fd85baeeae0daaae4c2e1f583
Vinay Kushwah <vk0475020@gmail.com>
2026-07-25 22:48:05 +0530

    Time: 1 ms (99.96%), Space: 46.7 MB (92.95%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            ans=ans^nums[i];//xor of same number is zero
+        }
+        return ans;
+    }
+}
