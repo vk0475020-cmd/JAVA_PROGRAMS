eb831926bb8751f0fd4f80e3e563adb8adba627c
Vinay Kushwah <vk0475020@gmail.com>
2026-07-29 00:33:16 +0530

    Time: 31 ms (63.05%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,3 +1,11 @@
+// ~we use the palindrome property if half of the palindrom is same to it mirror
+    // example daccad    half=>dac | cad    -->daccad     
+    //                         (mirror)
+    // ~convert string to char Array for convinence
+    // ~then we sort the left half 
+    // ~left half copy kar denge last me 
+    // ~last me string type return kar do 
+    //APPROACH
 class Solution {
     public String smallestPalindrome(String s) {
         char chararr[] = s.toCharArray();
@@ -6,7 +14,7 @@ class Solution {
         Arrays.sort(chararr,0,mid);
         for(int i=0;i<mid ;i++){
             chararr[l-1-i]=chararr[i];
-        }
-        return new String(chararr);
+        }return new String(chararr);
     }
+    
 }
