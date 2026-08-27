e936d8da02fd3a29aacb54b59044047c9be82dc2
Vinay Kushwah <vk0475020@gmail.com>
2026-08-28 02:06:21 +0530

    Time: 1 ms (90.98%), Space: 43.6 MB (36.19%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -4,9 +4,11 @@ class Solution {
         int l=columnTitle.length();
         for(int i=l-1;i>=0;i--){
             int temp=columnTitle.charAt(i)-64;
-            s=s+(int)Math.pow(26,p++)*temp;
-
+            s=s+(int)Math.pow(26,p++)*temp; //Math funtion give's double so be have to convert it
         }
         return s;
+        //AB =>1*26^1 + 2*26^0 =>26+2=>28 something like binary to int convergen 
+        // 26 ? =>alphabet(a-z) count
+        //ASCII code => A->64
     }
 }
