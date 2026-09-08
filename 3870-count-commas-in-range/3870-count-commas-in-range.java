92848f028e3519fef77e152022440f312e0a5da1
Vinay Kushwah <vk0475020@gmail.com>
2026-09-08 23:19:58 +0530

    Time: 5 ms (12.12%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,7 +2,14 @@ class Solution {
     public int countCommas(int n) {
         if (n < 1000)
             return 0;
-
-        return n - 999;
+        int c=0;
+        for(int i=1000;i<=n;i++){
+            c++;
+        }
+        return c;
     }
+    //we have to count the no of , up to n
+    //   "1,000" ->1
+    // , "1,001" ->2
+    // , "1,002" ->3
 }
