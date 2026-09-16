6d7f6650ba5d43bc5341c43dfe7f8d8230a9c3e4
Vinay Kushwah <vk0475020@gmail.com>
2026-09-16 10:34:34 +0530

    Time: 5 ms (63.08%), Space: 44.3 MB (81.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        
+        if (m != n) {
+            return false;
+        }
+        
+        int[] count = new int[26];
+        for (int i = 0; i < m; i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
